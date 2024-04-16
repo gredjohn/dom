ca08df045e944bc766d84243acc3b18864485de5
greg <gwiechec@o2.pl>
2024-04-16 23:45:23 +0200

    Add photos

---
+++ virtual/data.js
@@ -0,0 +1,733 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-parter-wiatroap",
+      "name": "parter wiatrołap",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2244710480100096,
+          "pitch": 0.3065570366476251,
+          "rotation": 0,
+          "target": "2-parter-korytarz"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-parter-azienka",
+      "name": "parter łazienka",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6344687970168081,
+          "pitch": 0.4923926808045227,
+          "rotation": 0,
+          "target": "2-parter-korytarz"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-parter-korytarz",
+      "name": "parter korytarz",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8509288701357107,
+          "pitch": 0.4294623215049569,
+          "rotation": 0,
+          "target": "4-parter-garderoba-2"
+        },
+        {
+          "yaw": -1.592016244664336,
+          "pitch": 0.524454200045696,
+          "rotation": 0,
+          "target": "1-parter-azienka"
+        },
+        {
+          "yaw": -0.3266062743526206,
+          "pitch": 0.344443236194925,
+          "rotation": 0,
+          "target": "0-parter-wiatroap"
+        },
+        {
+          "yaw": -0.1451870307096499,
+          "pitch": 0.18525157221949762,
+          "rotation": 0,
+          "target": "15-parter-spiarnia"
+        },
+        {
+          "yaw": 0.042291704638191874,
+          "pitch": 0.15306174969135533,
+          "rotation": 0,
+          "target": "8-parter-kuchnia"
+        },
+        {
+          "yaw": 1.6483097676828855,
+          "pitch": 0.7384510681298249,
+          "rotation": 0,
+          "target": "14-schody-piwnica"
+        },
+        {
+          "yaw": 0.24432147404587568,
+          "pitch": 0.3242255225357873,
+          "rotation": 0,
+          "target": "13-schody"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-parter-garderoba",
+      "name": "parter garderoba",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3194352876726256,
+          "pitch": 0.8868207514389166,
+          "rotation": 0,
+          "target": "4-parter-garderoba-2"
+        },
+        {
+          "yaw": 2.3133822685812113,
+          "pitch": 0.27273088712177085,
+          "rotation": 0,
+          "target": "5-parter-gabinet"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-parter-garderoba-2",
+      "name": "parter garderoba 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.36747418313614943,
+          "pitch": 0.7366522976069465,
+          "rotation": 0,
+          "target": "3-parter-garderoba"
+        },
+        {
+          "yaw": 0.8178642534095015,
+          "pitch": 0.5072069945702715,
+          "rotation": 0,
+          "target": "2-parter-korytarz"
+        },
+        {
+          "yaw": 2.8939011562780745,
+          "pitch": 0.5324031226778771,
+          "rotation": 0,
+          "target": "5-parter-gabinet"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-parter-gabinet",
+      "name": "parter gabinet",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3425529931764153,
+          "pitch": 0.6831136958764059,
+          "rotation": 0,
+          "target": "4-parter-garderoba-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-parter-salon",
+      "name": "parter salon",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5227114765351999,
+          "pitch": 0.287795755469638,
+          "rotation": 0,
+          "target": "2-parter-korytarz"
+        },
+        {
+          "yaw": -0.16844505141152943,
+          "pitch": 0.22121983983539018,
+          "rotation": 0,
+          "target": "7-parter-jadalnia"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-parter-jadalnia",
+      "name": "parter jadalnia",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.20476301312893064,
+          "pitch": 0.2799396046646976,
+          "rotation": 0,
+          "target": "6-parter-salon"
+        },
+        {
+          "yaw": 0.726523693531302,
+          "pitch": 0.376592226971713,
+          "rotation": 0,
+          "target": "2-parter-korytarz"
+        },
+        {
+          "yaw": 1.4673706589517401,
+          "pitch": 0.3180660222586802,
+          "rotation": 0,
+          "target": "8-parter-kuchnia"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-parter-kuchnia",
+      "name": "parter kuchnia",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2514454564156967,
+          "pitch": 0.18378239095560467,
+          "rotation": 0,
+          "target": "7-parter-jadalnia"
+        },
+        {
+          "yaw": 0.7067765895101736,
+          "pitch": 0.22200689126514916,
+          "rotation": 0,
+          "target": "6-parter-salon"
+        },
+        {
+          "yaw": 1.0747360397985055,
+          "pitch": 0.4217241874915594,
+          "rotation": 0,
+          "target": "2-parter-korytarz"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-pitro-sypialnia",
+      "name": "piętro sypialnia",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.994119658459592,
+          "pitch": 0.4386084479273258,
+          "rotation": 0,
+          "target": "11-pitro-korytarz"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-pitro-azienka",
+      "name": "piętro łazienka",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.899587218258155,
+          "pitch": 0.3344734897201107,
+          "rotation": 0,
+          "target": "2-parter-korytarz"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-pitro-korytarz",
+      "name": "piętro korytarz",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.45090934816475503,
+          "pitch": 0.35191110469353504,
+          "rotation": 0,
+          "target": "10-pitro-azienka"
+        },
+        {
+          "yaw": 2.2220496544394504,
+          "pitch": 0.30509867556299497,
+          "rotation": 0,
+          "target": "9-pitro-sypialnia"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-pitro-korytarz-2",
+      "name": "piętro korytarz 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.284156288821002,
+          "pitch": 0.23135246742550208,
+          "rotation": 0,
+          "target": "10-pitro-azienka"
+        },
+        {
+          "yaw": -0.5950564934784843,
+          "pitch": 0.16237942320503507,
+          "rotation": 0,
+          "target": "9-pitro-sypialnia"
+        },
+        {
+          "yaw": -0.8134362006532783,
+          "pitch": 0.6605762171249996,
+          "rotation": 0,
+          "target": "12-pitro-korytarz-2"
+        },
+        {
+          "yaw": 2.4763901927144785,
+          "pitch": 1.0109492412500014,
+          "rotation": 0,
+          "target": "13-schody"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-schody",
+      "name": "schody",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5403514276980612,
+          "pitch": 0.19270277360453214,
+          "rotation": 0,
+          "target": "12-pitro-korytarz-2"
+        },
+        {
+          "yaw": 1.0456817542876422,
+          "pitch": 0.9238073339715847,
+          "rotation": 0,
+          "target": "2-parter-korytarz"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-schody-piwnica",
+      "name": "schody piwnica",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.987017526578402,
+          "pitch": 0.09107543607180801,
+          "rotation": 0,
+          "target": "2-parter-korytarz"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "15-parter-spiarnia",
+      "name": "parter spiżarnia",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.088771397187376,
+          "pitch": 0.5196310707302807,
+          "rotation": 0,
+          "target": "2-parter-korytarz"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
